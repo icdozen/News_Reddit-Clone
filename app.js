b688ef33dde04f4535107b6716cf08758df89734
icdozen <icdozen@gmail.com>
2016-04-26 10:19:10 -0400

    app js view post adjustment

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
 angular.module('flapperNews', [])
 .controller('MainCtrl', [
-  '$scope',
-  function($scope){
-    $scope.test = 'heyo';
-  }]);
-
-$scope.posts = [
-  'post 1',
-  'post 2',
-  'post 3',
-  'post 4',
-  'post 5'  
+'$scope',
+function($scope){
+  $scope.posts = [
+  {title: 'post 1', upvotes: 5},
+  {title: 'post 2', upvotes: 2},
+  {title: 'post 3', upvotes: 15},
+  {title: 'post 4', upvotes: 9},
+  {title: 'post 5', upvotes: 4}
 ];
+}]);
+
+// $scope.posts = [
+//   {title: 'post 1', upvotes: 5},
+//   {title: 'post 2', upvotes: 2},
+//   {title: 'post 3', upvotes: 15},
+//   {title: 'post 4', upvotes: 9},
+//   {title: 'post 5', upvotes: 4}
+// ];
