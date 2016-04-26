07e8d0ff56d3273580be553dbade7f21eca3b3e7
icdozen <icdozen@gmail.com>
2016-04-26 10:19:31 -0400

    app js view post adjustment

--- app.js
+++ app.js
@@ -10,11 +10,3 @@ function($scope){
   {title: 'post 5', upvotes: 4}
 ];
 }]);
-
-// $scope.posts = [
-//   {title: 'post 1', upvotes: 5},
-//   {title: 'post 2', upvotes: 2},
-//   {title: 'post 3', upvotes: 15},
-//   {title: 'post 4', upvotes: 9},
-//   {title: 'post 5', upvotes: 4}
-// ];
